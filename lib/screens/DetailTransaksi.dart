450cdff87589a238a75bee9939e0ebc80f2b7fdd
veninayuliya <veninayuliya32@gmail.com>
2021-06-02 10:39:03 +0700

    membuat halaman detail transaksi

---
+++ lib/screens/DetailTransaksi.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:sepurane_kasir/screens/RiwayatTransaksi.dart';
+
+class DetailTransaksi extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(60),
+        child: AppBar(
+          title: Column(
+            children: [
+              Text(
+                "Detail Transaksi\n2021-05-28",
+                style: TextStyle(fontSize: 18),
+              ),
+            ],
+          ),
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back),
+            onPressed: () {
+              Navigator.of(context).push(MaterialPageRoute(
+                  builder: (BuildContext context) => RiwayatTransaksi()));
+            },
+          ),
+        ),
+      ),
+      body: ListView(
+        children: [
+          Card(
+            child: Container(
+              padding: EdgeInsets.fromLTRB(7, 3, 7, 3),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Container(
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                              image: NetworkImage(
+                                  "https://awsimages.detik.net.id/community/media/visual/2020/08/18/nasi-goreng-pedas_43.jpeg?w=700&q=90"),
+                              fit: BoxFit.fitWidth,
+                            ),
+                          ),
+                          height: 80,
+                        ),
+                      ),
+                      Expanded(
+                        child: Container(
+                          child: Text(
+                            "Nasi Goreng",
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: Container(
+                          child: Text(
+                            "1x",
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: Container(
+                          child: Text(
+                            "Rp 12.000",
+                            textAlign: TextAlign.center,
+                            //style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.all(20),
+            alignment: Alignment.centerRight,
+            child: Text(
+              "Total : Rp 12.000",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/screens/RiwayatTransaksi.dart
+++ lib/screens/RiwayatTransaksi.dart
@@ -1,42 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:sepurane_kasir/main.dart';
+import 'package:sepurane_kasir/screens/DetailTransaksi.dart';
 
 class RiwayatTransaksi extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: Text(
-            "Riwayat Transaksi",
-            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-          ),
-          leading: IconButton(
-            icon: Icon(Icons.arrow_back),
-            onPressed: () {
-              Navigator.of(context).push(MaterialPageRoute(
-                    builder: (BuildContext context) => MyApp()));
-            },
-          ),
+      appBar: AppBar(
+        title: Text("Riwayat Transaksi"),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.of(context).push(
+                MaterialPageRoute(builder: (BuildContext context) => MyApp()));
+          },
         ),
-        body: ListView(
-          children: [
-            Card(
-              color: Colors.white,
-              elevation: 2.0,
-              child: ListTile(
-                leading: CircleAvatar(
-                  backgroundColor: Colors.yellow,
-                  child: Icon(Icons.date_range_rounded),
-                ),
-                title: Text(
-                  "2021-05-28",
-                ),
-                subtitle: Text("Total : Rp 12.000"),
+      ),
+      body: ListView(
+        children: [
+          Card(
+            color: Colors.white,
+            elevation: 2.0,
+            child: ListTile(
+              leading: CircleAvatar(
+                backgroundColor: Colors.yellow,
+                child: Icon(Icons.date_range_rounded),
               ),
-            )
-          ],
-        )
-        );
+              title: Text(
+                "2021-05-28",
+              ),
+              subtitle: Text("Total : Rp 12.000"),
+              onTap: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                    builder: (BuildContext context) => DetailTransaksi()));
+              },
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
